69a59ee409034917bce026f63730c5f693fb1a2b
Nishant5789 <94158059+Nishant5789@users.noreply.github.com>
2022-07-12 23:24:57 +0530

    Time: 519 ms (42.70%), Space: 10.2 MB (47.49%) - LeetHub

---
+++ 473-matchsticks-to-square/473-matchsticks-to-square.cpp
@@ -0,0 +1,54 @@
+class Solution {
+    int a,b,c,d;
+    bool fun(vector<int>& matchsticks,int i){
+        //Base Case
+        if(i==matchsticks.size()){
+            if(a==0 && b==0 && c==0 && d==0) return true;
+            else return false;
+        }
+        
+        if(matchsticks[i]<=a){
+            a-=matchsticks[i];
+            if(fun(matchsticks,i+1)) return true;
+            a+=matchsticks[i];      
+        }
+        
+        if(matchsticks[i]<=b){
+            b-=matchsticks[i];
+            if(fun(matchsticks,i+1)) return true;
+            b+=matchsticks[i];                   
+        }
+        
+        if(matchsticks[i]<=c){
+            c-=matchsticks[i];
+            if(fun(matchsticks,i+1)) return true;
+            c+=matchsticks[i];
+        }
+        
+        if(matchsticks[i]<=d){
+            d-=matchsticks[i];
+            if(fun(matchsticks,i+1)) return true;
+            d+=matchsticks[i];       
+        }
+		
+
+        return false;
+    }
+public:
+    bool makesquare(vector<int>& matchsticks) {
+
+        if(matchsticks.size()<4) return false;
+        
+		// if sum of all number of array is not divisible by 4 , then we can not create a square
+		int sum = accumulate(matchsticks.begin(), matchsticks.end(),0);
+        if(sum % 4 != 0) return false;
+        
+		int sizeSum=sum/4;
+        a=sizeSum,b=sizeSum,c=sizeSum,d=sizeSum;
+        
+		// here we sort our array in reverse order to escape more cases
+		sort(matchsticks.rbegin(), matchsticks.rend());
+        
+		return fun(matchsticks,0);
+    }
+};
