dbb76a71706c142a032a28d56f610ba0339fd869
Nishant5789 <94158059+Nishant5789@users.noreply.github.com>
2022-07-30 14:34:40 +0530

    Time: 22 ms (29.84%), Space: 12.7 MB (10.40%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.cpp
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.cpp
@@ -6,33 +6,43 @@ public:
    stack<string> st;
         int ans = 0;
         int top1 = 0, top2 = 0;
-        for(int i=0;i<tokens.size();i++) {
-            if(tokens[i]!="+" and tokens[i]!="-" and tokens[i]!="*" and tokens[i]!="/" ){
+        for(int i=0;i<tokens.size();i++)
+        {
+            if(tokens[i]!="+" and tokens[i]!="-" and tokens[i]!="*" and tokens[i]!="/" )
+            {
                 st.push(tokens[i]);
             }
-            else {
+            else
+            {
                 if(!st.empty()) top1 = stoi(st.top());
                 st.pop();
                 if(!st.empty()) top2 = stoi(st.top());
                 st.pop();
-                if(tokens[i]=="+") {
+                if(tokens[i]=="+")
+                {
                     int num = top2 + top1;
                     // ans += num;
                     st.push(to_string(num));
                     // ans +=
                 }
-                else if(tokens[i]=="*") {
-                    int num = top2 * top1;
-                    st.push(to_string(num));
-                }
-                else if(tokens[i]=="/") {
-                    int num = top2/top1;
-                    st.push(to_string(num));
-                }
-                else if(tokens[i]=="-") {
-                    int num = top2 - top1;
-                    st.push(to_string(num));
-                }
+                else 
+                    if(tokens[i]=="*")
+                    {
+                        int num = top2 * top1;
+                        st.push(to_string(num));
+                    }
+                else
+                    if(tokens[i]=="/") 
+                    {
+                        int num = top2/top1;
+                        st.push(to_string(num));
+                    }
+                else 
+                    if(tokens[i]=="-")
+                    {
+                        int num = top2 - top1;
+                        st.push(to_string(num));
+                    }
             }
         }
         while(!st.empty()) {
