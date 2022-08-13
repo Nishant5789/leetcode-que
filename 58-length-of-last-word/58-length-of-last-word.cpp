3deb58942b417a6bb41d71bb90924238b6f4fa5b
Nishant5789 <94158059+Nishant5789@users.noreply.github.com>
2022-08-13 12:34:15 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (9.98%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.cpp
+++ 58-length-of-last-word/58-length-of-last-word.cpp
@@ -4,19 +4,22 @@ public:
         
         int end = s.length()-1;
         int length=0;
-        while(end!=0)
+        while(end!=0){
         if(s.at(end)==' ')
             end--;
-        else
-            break;
-        if(end==0)
-            return 1;
-         while(s.at(end)!= ' ' && end!=0){ 
-                length++;
-                end--;
-            }
-        if(end==0 && s.at(end)!=' ')
+            
+         if(s.at(end)!= ' ' && end!=0)
+         { 
+            length++;
+            end--;
+         
+            if(s.at(end)==' ' || end==0) 
+                break;
+         }   
+        }
+        if(s.at(end)!=' ' )
             return length+1;
         return length;
+
     }
 };
