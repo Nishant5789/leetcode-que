0bc750b6be6beb01eb79edd37ccd14fda787d623
Nishant Bhandari <94158059+Nishant5789@users.noreply.github.com>
2022-08-14 18:38:19 +0530

    Time: 217 ms (10.68%), Space: 59.7 MB (31.38%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
-        int n=arr.size(), l=0, h=n-1, res=-1;
-        while(l<=h)
+        int n=arr.size(), l=0, r=n-1;
+        
+        while(l<r)
         {
-            int mid=(l+h)/2;
-            if(arr[mid-1]<arr[mid] && arr[mid]>arr[mid+1])
-            {
-                res=mid;
-                break;
-            }
-            else 
-                if(arr[mid-1]<arr[mid] && arr[mid]<arr[mid+1])
-            {
-                l=mid;
-            }
+            int mid=l+(r-l)/2;
+            // why this approach here we have multiple option for which side we choose either left side or right side if we used only two condition
+            // so we need to anaylysis something
+            // we have four option d means decresing and i means incresing and middle index is mid formula for nibary search
+            //(1)-III (2) ddd (3)idi (4) did
+            // (1)(3) option we used l = mid + 1(why)
+            // (2)(4) option we used l = mid + 1
+            
+            // if we find the peak element but we don't stop the loop because there multiple peak exits
+            if(arr[mid] > arr[mid+1])
+                r = mid;
             else
-            {
-                h=mid;
-            }
+                l = mid+1;
         }
-        return res;
+        return l;
     }
 };
