8624652956509d4726ca4a6a4967db085e717402
Nishant Bhandari <94158059+Nishant5789@users.noreply.github.com>
2022-08-14 18:22:17 +0530

    Time: 8 ms (40.42%), Space: 8.8 MB (89.58%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cpp
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -6,6 +6,14 @@ public:
         while(l<r)
         {
             int mid=l+(r-l)/2;
+            // why this approach here we have multiple option for which side we choose either left side or right side if we used only two condition
+            // so we need to anaylysis something
+            // we have four option d means decresing and i means incresing and middle index is mid formula for nibary search
+            //(1)-III (2) ddd (3)idi (4) did
+            // (1)(3) option we used l = mid + 1(why)
+            // (2)(4) option we used l = mid + 1
+            
+            // if we find the peak element but we don't stop the loop because there multiple peak exits
             if(arr[mid] > arr[mid+1])
                 r = mid;
             else
